44c47a31b8d3ace692c93e23dd293c02a7b85079
9thCore <38700349+9thCore@users.noreply.github.com>
2025-12-01 08:38:24 +0200

    Solve 2025.1.1

---
+++ 2025/day1/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARBITRARY_INPUT_MAX_LENGTH 4200
+#define FULL_ROTATION 100
+#define RELEVANT_ROTATION 0
+
+typedef struct {
+    int current_rotation;
+} program_state;
+
+typedef struct {
+    int change;
+} program_input;
+
+void part1(program_state, program_input *, int);
+
+int main(int argc, char **argv)
+{
+    program_state initial_state = {FULL_ROTATION / 2};
+    program_input input[ARBITRARY_INPUT_MAX_LENGTH];
+    int input_length;
+
+    char direction, discard;
+    int change;
+
+    while (scanf("%c%d", &direction, &change) != EOF) {
+        input[input_length++].change = change * (direction == 'L' ? -1 : 1);
+
+        // Discard trailing newline
+        scanf("%c", &discard);
+    }
+
+    part1(initial_state, input, input_length);
+
+    return EXIT_SUCCESS;
+}
+
+void part1(program_state state, program_input *input, int input_length) {
+    int password = 0;
+
+    for (int i = 0; i < input_length; i++) {
+        state.current_rotation = (state.current_rotation + input[i].change) % FULL_ROTATION;
+        password += !state.current_rotation;
+    }
+
+    printf("The answer to part 1 is: %d\n", password);
+}
