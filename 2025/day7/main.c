07d76831c63fb3bcf7629d827f514ff31ddd438f
9thCore <38700349+9thCore@users.noreply.github.com>
2025-12-07 10:10:14 +0200

    Solve 2025.7.1

---
+++ 2025/day7/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ARBITRARY_LINE_COUNT 150
+#define ARBITRARY_LINE_LENGTH 150
+
+typedef unsigned long long number_t;
+#define NUMBER_FORMAT "%llu"
+
+typedef struct {
+    char manifold[ARBITRARY_LINE_COUNT][ARBITRARY_LINE_LENGTH];
+    number_t width, height;
+    number_t startx, starty;
+} program_input;
+
+void part1(program_input);
+void part2(program_input);
+
+int main(int argc, char **argv)
+{
+    program_input input = {.height = 0};
+
+    while (scanf("%s", input.manifold[input.height]) != EOF) {
+        char *p = strchr(input.manifold[input.height], 'S');
+        if (p != NULL) {
+            input.startx = p - input.manifold[input.height];
+            input.starty = input.height;
+        }
+
+        input.height++;
+    }
+
+    input.width = strlen(input.manifold[0]);
+
+    part1(input);
+    part2(input);
+
+    return EXIT_SUCCESS;
+}
+
+void part1(program_input input) {
+    number_t answer = 0;
+
+    int *tachyons = calloc(input.width, sizeof(int));
+
+    tachyons[input.startx] = 1;
+    for (int i = input.starty; i < input.height; i++) {
+        for (int j = 1; j < input.width - 1; j++) {
+            if (tachyons[j] && input.manifold[i][j] == '^') {
+                answer++;
+                tachyons[j] = 0;
+                tachyons[j - 1] = 1;
+                tachyons[j + 1] = 1;
+            }
+        }
+    }
+
+    free(tachyons);
+
+    printf("The answer to part 1 is: " NUMBER_FORMAT "\n", answer);
+}
+
+void part2(program_input input) {
+    number_t answer = 0;
+
+    printf("The answer to part 2 is: " NUMBER_FORMAT "\n", answer);
+}
